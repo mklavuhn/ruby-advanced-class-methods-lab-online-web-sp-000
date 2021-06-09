f6a4b88efa267e8efcf9f7c69993424f2e450dd2
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-09 19:56:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   def self.new_from_filename
     song = self.new 
     song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - "))
+    song.artist_name = (name.split(" - ")[0])
   
   def self.destroy_all
     @@all = []
